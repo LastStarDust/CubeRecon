dc70dbd196be3fcc8572617ac8e898f3f2fa3a45
Clark McGrew <clark.mcgrew@stonybrook.edu>
2020-09-20 14:48:09 -0400

    Make the log a tiny bit fancier so that log levels mean something.

--- io/CubeLog.hxx
+++ io/CubeLog.hxx
@@ -1,17 +1,20 @@
+#include <iostream>
+#include <iomanip>
+
 #ifndef CUBE_LOG_LEVEL
 #define CUBE_LOG_LEVEL 1
 #endif
 
 #ifndef CUBE_LOG
-#define CUBE_LOG(level) if ((level) <= (CUBE_LOG_LEVEL)) std::cout
+#define CUBE_LOG(level) if ((level) <= (CUBE_LOG_LEVEL)) std::cout << std::setfill('%') << std::setw(level+2) << " " << std::setfill(' ')
 #endif
 
 #ifndef CUBE_DEBUG_LEVEL
-#define CUBE_DEBUG_LEVEL 2
+#define CUBE_DEBUG_LEVEL 1
 #endif
 
 #ifndef CUBE_DEBUG
-#define CUBE_DEBUG(level) if ((level) <= (CUBE_DEBUG_LEVEL)) std::cout
+#define CUBE_DEBUG(level) if ((level) <= (CUBE_DEBUG_LEVEL)) std::cout << std::setfill('#') << std::setw(level+2) << " " << std::setfill(' ') << "(" << __LINE__ << "):"
 #endif
 
 #ifndef CUBE_ERROR
