7b17364393e0c115c19fda8b88a662cd12ed83a0
Clark McGrew <clark.mcgrew@stonybrook.edu>
2020-10-15 10:19:41 -0400

    Add tools to find truth information about a cube.
    This calculates the total energy, cross-talk, and the cube time based on the
    G4Hit objects contained in the cube.

---
+++ tools/ToolCubeTruth.cxx
@@ -0,0 +1,101 @@
+#include "ToolCubeTruth.hxx"
+#include "ToolG4Hits.hxx"
+
+#include <CubeEvent.hxx>
+#include <CubeHit.hxx>
+#include <CubeHandle.hxx>
+#include <CubeUnits.hxx>
+
+#include <vector>
+
+// Find the total energy that was deposited inside a cube.  (This should
+// include crosstalk).
+double Cube::Tool::CubeDeposit(Cube::Event& event,
+                               Cube::Handle<Cube::Hit> hit) {
+    std::vector<Cube::Handle<Cube::G4Hit>> hits
+        = Cube::Tool::HitG4Hits(event,hit);
+
+    double size = 10.0*unit::mm;
+    try {size = hit->GetProperty("Size");}
+    catch (...) {size = 10.0*unit::mm;}
+
+    double deposit = 0.0;
+    for (std::vector<Cube::Handle<Cube::G4Hit>>::iterator h = hits.begin();
+         h != hits.end(); ++h) {
+        TVector3 avg = 0.5*((*h)->GetStart().Vect() + (*h)->GetStop().Vect());
+        double v = std::abs(hit->GetPosition().X() - avg.X());
+        if (v > 0.5*size) continue;
+        v = std::abs(hit->GetPosition().Y() - avg.Y());
+        if (v > 0.5*size) continue;
+        v = std::abs(hit->GetPosition().Z() - avg.Z());
+        if (v > 0.5*size) continue;
+        deposit += (*h)->GetEnergyDeposit();
+    }
+
+    return deposit;
+}
+
+double Cube::Tool::CubeCrossTalk(Cube::Event& event,
+                                 Cube::Handle<Cube::Hit> hit) {
+    std::vector<Cube::Handle<Cube::G4Hit>> hits
+        = Cube::Tool::HitG4Hits(event,hit);
+
+    double size = 10.0*unit::mm;
+    try {size = hit->GetProperty("Size");}
+    catch (...) {size = 10.0*unit::mm;}
+
+    double deposit = 0.0;
+    for (std::vector<Cube::Handle<Cube::G4Hit>>::iterator h = hits.begin();
+         h != hits.end(); ++h) {
+        TVector3 avg = 0.5*((*h)->GetStart().Vect() + (*h)->GetStop().Vect());
+        double v = std::abs(hit->GetPosition().X() - avg.X());
+        if (v < 0.5*size) continue;
+        v = std::abs(hit->GetPosition().Y() - avg.Y());
+        if (v < 0.5*size) continue;
+        v = std::abs(hit->GetPosition().Z() - avg.Z());
+        if (v < 0.5*size) continue;
+        deposit += (*h)->GetEnergyDeposit();
+    }
+
+    return deposit;
+}
+
+
+// Find the time that the cube is "hit".  This is the time that should be
+// reconstructed for the cube.  It's close to the earliest hit segment time
+// (but very low energy segments are ignored).  This treats direct energy and
+// cross talk the same way, so the hit time may come from the large energy
+// deposition neighbor.  If the cube doesn't get over the threshold, then the
+// time of the last g4 hit is used.
+double Cube::Tool::CubeTime(Cube::Event& event,
+                            Cube::Handle<Cube::Hit> hit,
+                            double threshold) {
+    std::vector<Cube::Handle<Cube::G4Hit>> hits
+        = Cube::Tool::HitG4Hits(event,hit);
+
+    std::vector<std::pair<double,double>> timeEnergy;
+    for (std::vector<Cube::Handle<Cube::G4Hit>>::iterator h = hits.begin();
+         h != hits.end(); ++h) {
+        timeEnergy.push_back(std::make_pair((*h)->GetStart().T(),
+                                            (*h)->GetEnergyDeposit()));
+    }
+
+    if (timeEnergy.empty()) return 1E+20;
+
+    double enr = 0.0;
+    std::sort(timeEnergy.begin(), timeEnergy.end());
+    for (std::vector<std::pair<double,double>>::iterator
+             tt = timeEnergy.begin();
+         tt != timeEnergy.end(); ++tt) {
+        enr += tt->second;
+        if (enr > threshold) return tt->first;
+    }
+
+    return timeEnergy.back().first;
+}
+
+// Local Variables:
+// mode:c++
+// c-basic-offset:4
+// compile-command:"$(git rev-parse --show-toplevel)/build/cube-build.sh force"
+// End:
---
+++ tools/ToolCubeTruth.hxx
@@ -0,0 +1,29 @@
+#ifndef ToolCubeTruth_hxx_seen
+#define ToolCubeTruth_hxx_seen
+
+#include <CubeEvent.hxx>
+#include <CubeHit.hxx>
+#include <CubeHandle.hxx>
+
+namespace Cube {
+    namespace Tool {
+        /// Sum the true deposit for a cube.  This eliminates crosstalk hits.
+        double CubeDeposit(Cube::Event& event, Cube::Handle<Cube::Hit> hit);
+
+        /// Sum the true deposit for a cube coming from cross talk.
+        double CubeCrossTalk(Cube::Event& event, Cube::Handle<Cube::Hit> hit);
+
+        /// Determine the true time that the cube was hit.  The time is based
+        /// on when it crosses a certain energy deposition threshold.
+        double CubeTime(Cube::Event& event, Cube::Handle<Cube::Hit> hit,
+                        double threshold = 1.5 /*MeV*/);
+
+    }
+}
+#endif
+
+// Local Variables:
+// mode:c++
+// c-basic-offset:4
+// compile-command:"$(git rev-parse --show-toplevel)/build/cube-build.sh force"
+// End:
